545e87bc2f99a74cb4fee80dd71f313b8ff3651f
slickstef11 <stefan@wundergraph.com>
2023-02-08 14:33:40 -0500

    commit yolo

--- .wundergraph/operations/users/get.ts
+++ .wundergraph/operations/users/get.ts
@@ -1,14 +1,20 @@
 import { createOperation, z } from '../../generated/wundergraph.factory';
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+const filePath = path.join(process.cwd(), 'operations/users/db.json');
+const defaultUser = (id) => ({
+ id,
+ name: 'Default user',
+ bio: 'Default user return if user does not exist.'
+});
 
 export default createOperation.query({
 	input: z.object({
 		id: z.string(),
 	}),
 	handler: async ({ input }) => {
-		return {
-			id: input.id,
-			name: 'Jens',
-			bio: 'Founder of WunderGraph',
-		};
-	},
+		const json = JSON.parse(await readFile(filePath, 'utf8'));
+			return json[input.id] || defaultUser(input.id);
+	}
 });
--- .wundergraph/operations/users/update.ts
+++ .wundergraph/operations/users/update.ts
@@ -1,14 +1,30 @@
 import { createOperation, z } from '../../generated/wundergraph.factory';
+import {readFile, writeFile } from "fs/promises";
+import path from "path";
 
+const filePath = path.join(process.cwd(), 'operations/users/db.json');
+const defaultBio = 'This is a default bio.'
+const defaultName = 'This is a default name.'
 export default createOperation.mutation({
-	input: z.object({
-		id: z.string(),
-		name: z.string(),
-		bio: z.string(),
-	}),
-	handler: async ({ input }) => {
-		return {
-			...input,
-		};
-	},
+ input: z.object({
+  id: z.string(),
+  name: z.string(),
+  bio: z.string(),
+ }),
+ handler: async ({ input }) => {
+  let json = JSON.parse(await readFile(filePath, 'utf8'));
+  json = json || {users: {}};
+  const user = {
+   id: input.id,
+   name: defaultName,
+   bio: defaultBio,
+   ...input,
+  };
+  json[input.id] = user;
+  await writeFile(filePath, JSON.stringify(json));
+  return {
+   message: 'User updated.',
+   user: user,
+  }
+ },
 });
